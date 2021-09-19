c4c0360d4a1fab1fc804ae5ee3840066effadd57
Salwa-Mansour <alqadasisal@gmail.com>
2021-09-19 11:39:20 +0300

    kata solved 20

--- 7kata1st.js
+++ 7kata1st.js
@@ -43,5 +43,56 @@ function getMeSum(a,b){
   }
   return sum;
 }
-console.log('the sum is : '+getMeSum(1,3));
+// console.log('the sum is : '+getMeSum(1,3));
 let arr = ['a','b','b','j']
+function highAndLow(numbers){
+  // ...
+  arr=numbers.split(' ')
+ 
+ return `${Math.min(...arr)} ${Math.max(...arr)}`;
+}
+// console.log(highAndLow("1 2 3 4 5"));
+const intDiff = (arr, n) => {
+  // your code goes here
+  let counter=0;
+
+   for(let k=0;k<arr.length;k++){
+      console.log(arr[k]+" auter*******")
+        let i=0
+      while(i<arr.length){
+        console.log(i+' inner')
+        if(n!==0){
+            if(Math.abs(arr[i]-arr[k])===n) {
+              // console.log(arr[i]+" "+arr[k])
+          counter++;    }
+          }else{
+            if(arr[i]==arr[k] && k!=i){
+              counter++
+             
+            }
+            
+          }
+      i++;
+      }
+    }
+ 
+   return counter/2;
+}
+// console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 0))
+// console.log(intDiff([1, 1, 3, 3], 0))
+function lastSurvivor(letters, coords) {
+  let lastChar= letters.split('');
+  let noDuplicateCoords = [...new Set(coords)];
+for(let i=0;i<noDuplicateCoords.length;i++){
+  console.log(letters.split(""))
+  // [1,2,3,4].splice(0,0,)
+lastChar.splice(noDuplicateCoords[i],1,"");
+console.log(lastChar
+  )
+
+  }
+return String(lastChar.filter(e=>e));
+}
+// console.log(lastSurvivor('abc', [1,1]))
+// console.log(''==='')
+console.log(['4','7','2','71','30'].sort())
